d14f185c8a0c5ea6b600a6dc3d4b41dd60f587ef
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-04-02 20:27:10 -0300

    todo o service de games esta feito

--- backend/src/services/games.service.js
+++ backend/src/services/games.service.js
@@ -16,10 +16,29 @@ const createGame = async (id, score, userId) => {
     const games = await Game.create ({id, score, userId});
 
     return games;
+};
+
+const updateGame = async (id, score, userId) => {
+    const [updateGame] = await Game.update({ score, userId}, {
+        where: { id }
+    });
+
+    return updateGame;
+
 }
+
+const deleteGame = async (id) => {
+    const games = await Game.destroy({ where: { id } });
+
+    return games;
+};
+
+
 
 module.exports = {
     getAllGames,
     getGamesById,
     createGame,
+    deleteGame,
+    updateGame,
 };
