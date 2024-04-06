c39b0fdfa28518f89a737f04fe85c5d02b31a8a2
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-04-06 10:59:50 -0300

    corrigindo relacionamentos entre tabelas

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,13 +3,26 @@ const sequelize = require('../config/config');
 
 const UserModel = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        full_name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        contact: DataTypes.STRING
+        full_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        contact: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
     }, {
         tableName: 'Users',
         timestamps: false
     });
+
+    User.hasMany(Game); // Um usuário pode ter muitos jogos
+    User.hasMany(Answer); // Um usuário pode ter muitas respostas, qdo tiver
 
     return User;
 };
